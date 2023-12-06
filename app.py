7294b4d51a2fc52eef6011f2c640ce4f0f731d16
BMustafa97 <bilalmustafa1797@gmail.com>
2023-12-06 10:09:18 +0000

    loading bar added

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -13,6 +14,20 @@ def home():
 
 @app.route('/test', methods=['GET', 'POST'])
 def test():
+    if request.method == 'POST':
+        url = request.form['url']
+        # check if the url is valid
+        if url:
+            # check it starts with http:// or https://
+            if url.startswith('http://') or url.startswith('https://'):
+                print(url)
+                return render_template('home.html', url=url)
+            else:
+                url = 'https://' + url
+                print(url)
+                return render_template('home.html', url=url)
+
+        return render_template('home.html')
     return render_template('test.html')
 
 if __name__ == '__main__':
